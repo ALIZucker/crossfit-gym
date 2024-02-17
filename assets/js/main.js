cc7642de2c7baaf14fbcd83ec6569ff32a3db2e5
ALI_Zucker <zabihiali1998@gmail.com>
2024-02-17 20:11:43 +0330

    day3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 const img = document.getElementById("main-img2");
 const h1 = document.getElementById("main-img-h1");
 const pslider = document.getElementById("main-img-p");
+const containser = document.getElementById("showService");
+
 
 //--------------------------------------
 const datasSlider = [{
@@ -29,61 +31,150 @@ setInterval(function () {
 }, 2600);
 
 //------------------------------------------------------------
+const dataService = [
+    {
+        src: "assets/img/services-icon-1.png",
+        titr: "Weight Lifting",
+        desc: "A CrossFit gym, often referred to as a \"box,\" is a facility where individuals can participate in CrossFit workouts led by certified coaches."
+    },
+    {
+        src: "assets/img/services-icon-2.png",
+        titr: "Running",
+        desc: "A CrossFit gym, often referred to as a \"box,\" is a facility where individuals can participate in CrossFit workouts led by certified coaches."
+    },
+    {
+        src: "assets/img/services-icon-3.png",
+        titr: "Yoga Meditation",
+        desc: "A CrossFit gym, often referred to as a \"box,\" is a facility where individuals can participate in CrossFit workouts led by certified coaches."
+    },
+    {
+        src: "assets/img/services-icon-4.png",
+        titr: "Body Building",
+        desc: "A CrossFit gym, often referred to as a \"box,\" is a facility where individuals can participate in CrossFit workouts led by certified coaches."
+    },
+]
+for (let x of dataService) {
+    containser.append(createostad(x.src, x.titr, x.desc))
+}
+const classarticle = document.querySelectorAll(".articleserv");
+const h3service = document.querySelectorAll(".h3serv");
+
 $(document).ready(function () {
     $("#post1").on({
         mouseenter: function () {
             $(this).css({transform: 'scale(1.1,1.1)'});
-            $("#post1 > h2").css({color:"#efbf01"});
-            $("#post1 > p").css({color:"#efbf01"});
+            $("#post1 > h2").css({color: "#efbf01"});
+            $("#post1 > p").css({color: "#efbf01"});
         },
         mouseleave: function () {
             $(this).css({transform: 'scale(1,1)'});
             console.log("asdasdasdasd444asd");
-            $("#post1 > h2").css({color:'white'});
-            $("#post1 > p").css({color:"white"});
+            $("#post1 > h2").css({color: 'white'});
+            $("#post1 > p").css({color: "white"});
         },
 
     });
     $("#post2").on({
         mouseenter: function () {
             $(this).css({transform: 'scale(1.1,1.1)'});
-            $("#post2 > h2").css({color:"#efbf01"});
-            $("#post2 > p").css({color:"#efbf01"});
+            $("#post2 > h2").css({color: "#efbf01"});
+            $("#post2 > p").css({color: "#efbf01"});
         },
         mouseleave: function () {
             $(this).css({transform: 'scale(1,1)'});
             console.log("asdasdasdasd444asd");
-            $("#post2 > h2").css({color:'white'});
-            $("#post2 > p").css({color:"white"});
+            $("#post2 > h2").css({color: 'white'});
+            $("#post2 > p").css({color: "white"});
         },
 
     });
     $("#post3").on({
         mouseenter: function () {
             $(this).css({transform: 'scale(1.1,1.1)'});
-            $("#post3 > h2").css({color:"#efbf01"});
-            $("#post3 > p").css({color:"#efbf01"});
+            $("#post3 > h2").css({color: "#efbf01"});
+            $("#post3 > p").css({color: "#efbf01"});
         },
         mouseleave: function () {
             $(this).css({transform: 'scale(1,1)'});
             console.log("asdasdasdasd444asd");
-            $("#post3 > h2").css({color:'white'});
-            $("#post3 > p").css({color:"white"});
+            $("#post3 > h2").css({color: 'white'});
+            $("#post3 > p").css({color: "white"});
         },
 
     });
     $("#post4").on({
         mouseenter: function () {
             $(this).css({transform: 'scale(1.1,1.1)'});
-            $("#post4 > h2").css({color:"#efbf01"});
-            $("#post4 > p").css({color:"#efbf01"});
+            $("#post4 > h2").css({color: "#efbf01"});
+            $("#post4 > p").css({color: "#efbf01"});
         },
         mouseleave: function () {
             $(this).css({transform: 'scale(1,1)'});
             console.log("asdasdasdasd444asd");
-            $("#post4 > h2").css({color:'white'});
-            $("#post4 > p").css({color:"white"});
+            $("#post4 > h2").css({color: 'white'});
+            $("#post4 > p").css({color: "white"});
         },
 
     });
+    $("#right-blog >a").click(function () {
+        $("#hideme").show(1000).fadeIn(2000)
+    });
+    $(classarticle[0]).on({
+        mouseenter: function () {
+            $(h3service[0]).css({color: "#efbf01"});
+        },
+        mouseleave: function () {
+            $(h3service[0]).css({color: 'black'});
+
+        },
+
+    });
+    $(classarticle[1]).on({
+        mouseenter: function () {
+            $(h3service[1]).css({color: "#efbf01"});
+        },
+        mouseleave: function () {
+            $(h3service[1]).css({color: 'black'});
+
+        },
+
+    });
+    $(classarticle[2]).on({
+        mouseenter: function () {
+            $(h3service[2]).css({color: "#efbf01"});
+        },
+        mouseleave: function () {
+            $(h3service[2]).css({color: 'black'});
+
+        },
+
+    });
+    $(classarticle[3]).on({
+        mouseenter: function () {
+            $(h3service[3]).css({color: "#efbf01"});
+        },
+        mouseleave: function () {
+            $(h3service[3]).css({color: 'black'});
+
+        },
+
+    });
+
 });
+
+function createostad(strimg, strtitle, strp) {
+    const articleserv = document.createElement("article");
+    const imgserv = document.createElement("img");
+    const h3serv = document.createElement("h3");
+    const posserv = document.createElement("p");
+
+    articleserv.setAttribute("class", "articleserv");
+    imgserv.setAttribute("class", "imgserv");
+    h3serv.setAttribute("class", "h3serv");
+    posserv.setAttribute("class", "posserv");
+    imgserv.src = strimg;
+    h3serv.innerHTML = strtitle;
+    posserv.innerHTML = strp;
+    articleserv.append(imgserv, h3serv, posserv);
+    return articleserv;
+}
